529936aa3cad98ec1c77c7e6800d4569dcf80443
Korentinmassif <168642572+Korentinmassif@users.noreply.github.com>
2025-05-07 20:52:12 +0200

    Update fonction.lua

--- fonction.lua
+++ fonction.lua
@@ -14,11 +14,30 @@ display.scale = function(scale)
 	end
 end
 
-display.writeLine = function(line)
-	if display.monitor == nil then error("Set display before writing") end
-	_, y = display.monitor.getCursorPos()
-	display.monitor.write(line)
-	display.monitor.setCursorPos(1, y+1)
+display.writeLine = function(line, attibute)
+	if attribute = nil then
+		if display.monitor == nil then error("Set display before writing") end
+		_, y = display.monitor.getCursorPos()
+		display.monitor.write(line)
+		display.monitor.setCursorPos(1, y+1)
+	elseif attribute = "centered" then
+		if display.monitor == nil then error("Set display before writing") end
+		x, y = display.monitor.getCursorPos()
+		textLen = #line
+		noSpace = (x-textLen)%2
+		display.monitor.write(string.rep(" ", noSpace) .. line)
+		display.monitor.setCursorPos(1, y+1)
+	end
 end
 
+colony = nil
+colony = {}
+colony.colony = nil
 
+colony.setColony = function(colony)
+	colony.colony = colony
+end
+
+colony.getName = function()
+	return colony.colony.getName()
+end
