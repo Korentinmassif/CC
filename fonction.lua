ec89afdad3320e226473063354c7e07b603c45e7
Korentinmassif <168642572+Korentinmassif@users.noreply.github.com>
2025-05-07 19:19:19 +0200

    Update fonction.lua

--- fonction.lua
+++ fonction.lua
@@ -1,6 +1,10 @@
 display = {}
 display.monitor = nil
-display.writeLine = function (line)
+
+display.setMonitor = function(monitor)
+	display.monitor = monitor
+end
+display.writeLine = function(line)
 	if display.monitor == nil then error("Set display before writing") end
 	_, y = display.monitor.getCursorPos()
 	display.monitor.write(line)
