409a01657e73fbb12c3b8e4ad508936b7e51293f
Korentinmassif <168642572+Korentinmassif@users.noreply.github.com>
2025-05-13 13:49:31 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,10 +1,37 @@
 rednet.open("back")
 
+-- Fonction utilitaire pour jouer un son dans un thread séparé
+local function playURL(url)
+    parallel.waitForAny(
+        function()
+            shell.run("speaker play " .. url)
+        end,
+        function()
+            -- thread vide juste pour que parallel ne bloque pas
+        end
+    )
+end
+
+local function playerStop()
+    parallel.waitForAny(
+        function()
+            shell.run("speaker stop")
+        end,
+        function()
+            -- thread vide juste pour que parallel ne bloque pas
+        end
+    )
+end
+
 while true do
     local senderID, message = rednet.receive()
+    
     if message == "butterfly" then
-        shell.run("speaker play https://github.com/Korentinmassif/CC/raw/refs/heads/main/butterfly.dfpwm")
+        playURL("https://github.com/Korentinmassif/CC/raw/refs/heads/main/butterfly.dfpwm")
     elseif message == "bailando" then
-        shell.run("speaker play https://github.com/Korentinmassif/CC/raw/refs/heads/main/bailando.dfpwm")
+        playURL("https://github.com/Korentinmassif/CC/raw/refs/heads/main/bailando.dfpwm")
+    elseif message == "stop" then
+        playerStop()
     end
+
 end
