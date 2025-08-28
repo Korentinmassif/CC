521e7bbcc86d1a002b5a5f30171b6a4e4c91eed8
Korentinmassif <168642572+Korentinmassif@users.noreply.github.com>
2025-08-28 23:49:12 +0200

    Update uti.lua

--- Code/Utilities/uti.lua
+++ Code/Utilities/uti.lua
@@ -1,18 +1,20 @@
-return {
+uti = {
     istable = function(item)
         return type(item)=="table"
     end,
 
     lprint = function(item, index)
-        if istable(item) then 
+        if uti.istable(item) then 
             for k, v in pairs(item) do 
                 if istable(v) then 
                     print(string.rep("\t", index)..k)
-                    lprint(v, index + 1)
+                    uti.lprint(v, index + 1)
                 else
                     print(string.rep("\t", index)..k, string.rep("\t", index)..v)
                 end
             end
         else 
         print(string.rep("\t", index)..item)
+        end
+    end
 }
