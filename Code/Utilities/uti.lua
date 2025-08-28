19217bc1d2604083b95ba8d035b2f8ef3f34d2ee
Korentinmassif <168642572+Korentinmassif@users.noreply.github.com>
2025-08-28 23:40:39 +0200

    Create uti.lua

---
+++ Code/Utilities/uti.lua
@@ -0,0 +1,18 @@
+return {
+    istable = function(item)
+        return type(item)=="table"
+    end,
+
+    lprint = function(item, index)
+        if istable(item) then 
+            for k, v in pairs(item) do 
+                if istable(v) then 
+                    print(string.rep("\t", index)..k)
+                    lprint(v, index + 1)
+                else
+                    print(string.rep("\t", index)..k, string.rep("\t", index)..v)
+                end
+            end
+        else 
+        print(string.rep("\t", index)..item)
+}
