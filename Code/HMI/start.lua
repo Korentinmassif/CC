88a43cd1ac81d20d8f23b6dbfccc55c963b626fe
Korentinmassif <168642572+Korentinmassif@users.noreply.github.com>
2025-08-29 19:01:58 +0200

    Update start.lua

--- Code/HMI/start.lua
+++ Code/HMI/start.lua
@@ -10,10 +10,11 @@ while true do
     if state == machine.init then 
         monitor = Monitor.new()
         state = machine.start
-    else if state == machine.start then
+    elseif state == machine.start then
         window = require"PIC/main_menu.lua"
         monitor:loadConfig(window)
         monitor:draw()
     end
     os.wait(0.1)
 end
+
