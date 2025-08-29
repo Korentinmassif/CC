efa144d7ffde7d13756278d88a6e5f395f201db2
Korentinmassif <168642572+Korentinmassif@users.noreply.github.com>
2025-08-29 17:30:18 +0200

    Update monitor.lua

--- Code/HMI/src/monitor.lua
+++ Code/HMI/src/monitor.lua
@@ -1,41 +1,43 @@
-require"./button.lua"
+require "./button.lua"
 
 Monitor = {}
 Monitor.__index = Monitor
 
-function Monitor.new(label)
+function Monitor.new()
     local self = setmetatable({}, Monitor)
     self.mon = peripheral.find("monitor")
     self.buttons = {}
     return self
 end
 
-Monitor:addButton(button)
-    table.insert(self.buttons, button)
+function Monitor:addButton(...)
+    table.insert(self.buttons, ...)
 end
 
-Monitor:draw()
-    for k, button in pairs(self.buttons) do
+function Monitor:draw()
+    self.mon.clear()
+    for _, button in pairs(self.buttons) do
+        local x, y = button.x, button.y
+        local width, height = button.width, button.height
+
         self.mon.setBackgroundColor(button.bdColor)
         for i = 0, height - 1 do
-            mon.setCursorPos(x, y + i)
-            mon.write((" "):rep(width))
+            self.mon.setCursorPos(x, y + i)
+            self.mon.write((" "):rep(width))
         end
 
-    -- Dessiner l'intérieur du bouton (1 pixel en moins sur chaque bord)
-        mon.setBackgroundColor(button.bgColor)
+        self.mon.setBackgroundColor(button.bgColor)
         for i = 1, height - 2 do
-            mon.setCursorPos(x + 1, y + i)
-            mon.write((" "):rep(width - 2))
+            self.mon.setCursorPos(x + 1, y + i)
+            self.mon.write((" "):rep(width - 2))
         end
 
-    -- Afficher le texte centré
         if button.label then
-            local labelX = x + math.floor((width - #label) / 2)
+            local labelX = x + math.floor((width - #button.label) / 2)
             local labelY = y + math.floor(height / 2)
-            mon.setCursorPos(labelX, labelY)
-            mon.setTextColor(button.txColor)
-            mon.write(label)
+            self.mon.setCursorPos(labelX, labelY)
+            self.mon.setTextColor(button.txColor)
+            self.mon.write(button.label)
         end
     end
 end
